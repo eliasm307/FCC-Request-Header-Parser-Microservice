5c6766e203a118e7bd314bda849534c2acb4be7d
eliasm307 <eliaschakomangoro@gmail.com>
2020-10-31 14:20:07 +0000

    FCC tests passing

--- server.js
+++ server.js
@@ -5,6 +5,9 @@
 var express = require('express');
 var app = express();
 
+//set JSON spaces for output // dont, this makes fcc tests fail
+// app.set('json spaces', 4)
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -13,15 +16,29 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+// log all requests
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
+ 
+// IP header information API endpoint
+app.get("/api/whoami", function (req, res) {   
 
+  const responseDetails = {
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"],
+  };
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  console.log({responseDetails});
+
+  res.json(responseDetails);
 });
 
 
